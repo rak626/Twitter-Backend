be1896d5df22c9f383c8e8bc3f689ed1cd3e8466
Rakesh Ghosh <rakeshacot@gmail.com>
2023-03-29 13:03:16 +0530

    Update AppConstants.java
    removed key

--- src/main/java/com/rakesh/TwitterBackend/utils/AppConstants.java
+++ src/main/java/com/rakesh/TwitterBackend/utils/AppConstants.java
@@ -4,7 +4,7 @@ public class AppConstants {
 
     public static final String DEFAULT_IMAGE = "default.png";
     public static final String OPENAI_API_URL = "https://api.openai.com/v1/completions";
-    public static final String OPENAI_API_KEY = "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";
+    public static final String OPENAI_API_KEY = "";
 
     public static final int MAX_TOKENS = 2048;
 }
